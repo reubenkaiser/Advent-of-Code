cfd8a12f8fd84ef9642c9d01af8f1be6503268f2
reubenkaiser <reuben.schndr123@gmail.com>
2022-12-03 18:53:59 +1100

    day3 part2 complete

--- day3/day3.py
+++ day3/day3.py
@@ -7,18 +7,11 @@ with open('input.txt') as input_file:
       
 
 #%%
-def part_one():
-    rucksack_overlap = []
-    for rucksack in rucksacks:
-        compartment_size = int(len(rucksack)/2)
-        compartment1 = set(rucksack[:compartment_size])
-        compartment2 = set(rucksack[compartment_size:])
-        overlap_item = compartment1.intersection(compartment2).pop()
-        rucksack_overlap.append(overlap_item)
+def total_priority(item_list):
+    '''Returns the sum of priorities from a list of item codes.'''
+    total = 0
 
-    total_priority = 0
-
-    for item in rucksack_overlap:
+    for item in item_list:
         char_num = ord(item)
         item_priority = 0
 
@@ -27,11 +20,46 @@ def part_one():
         elif str.isupper(item):
             priority = char_num - 38
 
-        total_priority += priority    
-        
-    return total_priority
+        total += priority
+    
+    return total
+
+
+def part_one():
+    rucksack_overlap = []
+    for rucksack in rucksacks:
+        compartment_size = int(len(rucksack)/2)
+        compartment1 = set(rucksack[:compartment_size])
+        compartment2 = set(rucksack[compartment_size:])
+        overlap_item = compartment1.intersection(compartment2).pop()
+        rucksack_overlap.append(overlap_item)
+          
+    return total_priority(rucksack_overlap)
+
+def part_two():
+    group_starts = range(0, len(rucksacks), 3) # list of points where groups start in input
+    
+    # list of grouped rucksacks
+    groups = []
+    for start in group_starts:
+        group = rucksacks[start:start+3]
+        groups.append(group)
+
+    # list of badge types from grouped rucksacks
+    group_badges = []
+    for group in groups:
+        group = [set(rucksack) for rucksack in group]
+        badge = group[0] & group[1] & group[2]
+        group_badges.append(badge.pop())
+    
+    return total_priority(group_badges)
+
 
 if __name__ == '__main__':
     print(part_one())
+    print(part_two())
+
+    
+
 
 # %%
