a79afa757a665d0bb88be77a3d1402f482cdb031
reubenkaiser <reuben.schndr123@gmail.com>
2022-12-04 23:16:13 +1100

    small tweaks to day4

--- day4.py
+++ day4.py
@@ -2,21 +2,29 @@
 from AoC import AoC
 session = AoC()
 
+# load data as a list of paired ranges
 with open('inputs/input4.txt') as input_file:
     paired_elf_duties = []
     for line in input_file:
         line = line.strip()
         pair = line.split(',')
         pair = [id_range.split('-') for id_range in pair]
-        pair = [range(int(id_range[0]), int(id_range[1]) + 1) for id_range in pair]
+        pair = [
+            set(
+                range(
+                    int(id_range[0])
+                    , int(id_range[1]) + 1
+                )
+            ) for id_range in pair
+        ]
         paired_elf_duties.append(pair)
 
 #%%
 def part_one():
     count = 0
     for pair in paired_elf_duties:
-        elf_one = set(pair[0])
-        elf_two = set(pair[1])
+        elf_one = pair[0]
+        elf_two = pair[1]
 
         if elf_one.issubset(elf_two) or elf_one.issuperset(elf_two):
             count += 1
@@ -27,8 +35,8 @@ def part_one():
 def part_two():
     count = 0
     for pair in paired_elf_duties:
-        elf_one = set(pair[0])
-        elf_two = set(pair[1])
+        elf_one = pair[0]
+        elf_two = pair[1]
 
         if not elf_one.isdisjoint(elf_two):
             count += 1
