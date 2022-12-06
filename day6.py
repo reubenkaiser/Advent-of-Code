bccfc2046d431baabb48ea234256f3e75ba7b12e
reubenkaiser <reuben.schndr123@gmail.com>
2022-12-06 20:58:58 +1100

    day6 complete

---
+++ day6.py
@@ -0,0 +1,21 @@
+#%%
+from AoC import AoC
+session = AoC()
+
+# %%
+with open('inputs/input6.txt') as input_file:
+    stream = input_file.readline().strip()
+
+# %%
+def process_stream(stream, offset):
+    for start in range(0, len(stream) - offset):
+        window = stream[start:start + offset]
+        if len(set(window)) == offset:
+            break
+    
+    return stream.find(window) + offset
+
+
+if __name__ == '__main__':
+    print(process_stream(stream, 4))
+    print(process_stream(stream, 14))
